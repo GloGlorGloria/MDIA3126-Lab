5baf548bac50ab684c6d0627fce527884e3ac2ef
GloGlorGloria <gloriabcit@gmail.com>
2024-09-17 15:34:47 -0700

    project setup and navigation tabs

---
+++ mobile-app/app/(tabs)/cheese.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native';
+import { Link } from 'expo-router';
+
+export default function cheese() {
+    return (
+    <View>
+         <Text> This is my 'cheese' page 🪤</Text>
+         <Link href="/sandwich">Sandwitch🍞</Link>
+    </View>
+   
+    )
+}
